79fe230aa293dc530294e44ca8875e98cfb7048f
Nasib <143509503+Nasibbabazadeh@users.noreply.github.com>
2024-10-21 21:14:57 +0400

    Delete dist directory

--- dist/angular-tailwind-user-app/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-D6UWGOX5.mjs";import"./chunk-HT4RKANN.mjs";import"./chunk-G627YMSH.mjs";import"./chunk-G44MNRR7.mjs";import"./chunk-KB3MMIXW.mjs";import"./chunk-J4BDXTMW.mjs";import"./chunk-SOET56QL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
