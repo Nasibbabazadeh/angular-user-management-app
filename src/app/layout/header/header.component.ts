e36c5043713322b830213b4d8d136d03a75bf37c
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-10-21 11:08:53 +0400

    header mobile-nav menu

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { RouterLink, RouterLinkActive } from '@angular/router'
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [RouterLinkActive, RouterLink],
+    imports: [RouterLinkActive, RouterLink, CommonModule],
     templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    isMenuOpen: boolean = false
+}
